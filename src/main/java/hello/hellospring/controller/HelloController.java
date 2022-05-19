63c85ab05132274db46c4fa66411ebc2470c6800
taechacode <kangtaechan2015@gmail.com>
2022-05-20 00:04:20 +0900

    Update Repository 14강 회원 서비스 테스트까지의 예제 코드 작성

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -26,5 +27,38 @@ public class HelloController {
 
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+
+    // HTTP의 Header와 Body 중 Body에 return 값을 직접 넣어주겠다는 의미
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+
+        // parameter로 spring을 넘겨줄 경우
+        // 결과 화면을 페이지 소스보기 해보면 HTML 태그는 사라지고 "hello spring" 밖에 없다.
+        return "hello " + name;
+    }
+    
+    // 'viewResolver' 대신에 'HttpMessageConverter'가 동작
+    // 객체는 기본적으로 JSON 방식으로 반환됨 (HttpMessageConverter에서 JsonConverter 동작)
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
---
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,90 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    // 테스트 코드는 빌드될 때 실제 배포 코드에 포함되지 않아서 한글로 써도 무방
+    void 회원가입() {
+
+        // given
+        Member member = new Member();
+        member.setName("hello");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+
+        // given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        // when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+        /*
+        try-catch 구문으로 예외를 잡아내는 방법
+
+        try {
+            memberService.join(member2); // member1과 같은 이름을 가진 member2를 join
+            fail(); // 중복 가입으로 인한 실패
+        } catch (IllegalStateException e) {
+            // 여기까지 오면 예외가 발생해서 정상적으로 동작한 것이다.
+            assertThat(e.getMessage()).isEqualTo("중복 예외 발생");
+        }
+
+        */
+
+
+        // then
+
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+    
+}
